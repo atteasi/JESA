e92049a558cb839acc5f4710023bd3b5dafb7d46
atteasi <atte.asikainen@metropolia.fi>
2023-01-10 10:50:31 +0200

    initial commit

--- projekti/main.java
+++ projekti/main.java
@@ -1,3 +1,5 @@
-public static void main(String[] args) {
-    System.out.println("Hello World! :D")        
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World! :D");        
+    }
 }
